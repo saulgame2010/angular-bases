61aeb8baf6c1f6457805c1764779d7daf674e074
saulgame2010 <saulgame2010@gmail.com>
2023-05-24 13:03:03 -0600

    Fin de sección 4

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+export class HeroComponent {
+  public name: string = 'ironman';
+  public age: number = 45;
+
+  get capitalizeName(): string {
+    return this.name.toUpperCase();
+  }
+
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    this.name = 'Saúl'
+  }
+
+  changeAge(): void {
+    this.age = 23;
+  }
+
+  reset(): void {
+    this.name = 'ironman';
+    this.age = 45;
+  }
+}
